72d6561ef56c2541a557d6e269ad1a3b1f201cdd
zhanzr <zhanzr@foxmail.com>
2019-06-29 11:51:50 +0800

    partially fixed.

--- RTE/RTOS/FreeRTOSConfig.h
+++ RTE/RTOS/FreeRTOSConfig.h
@@ -51,33 +51,33 @@ extern uint32_t SystemCoreClock;
 /* Constants that describe the hardware and memory usage. */
 #define configCPU_CLOCK_HZ                    (SystemCoreClock)
 #define configTICK_RATE_HZ                    ((TickType_t)1000)
-#define configTOTAL_HEAP_SIZE                 ((size_t)16*1024)
-#define configMINIMAL_STACK_SIZE              ((uint16_t)256)
+#define configTOTAL_HEAP_SIZE                 ((size_t)32*1024)
+#define configMINIMAL_STACK_SIZE              ((uint16_t)384)
 #define configSUPPORT_DYNAMIC_ALLOCATION      1
 #define configSUPPORT_STATIC_ALLOCATION       0
 
 /* Constants related to the behaviour or the scheduler. */
 #define configMAX_PRIORITIES                  5
 #define configUSE_PREEMPTION                  1
-#define configUSE_TIME_SLICING                1
+#define configUSE_TIME_SLICING                0
 #define configIDLE_SHOULD_YIELD               1
-#define configMAX_TASK_NAME_LEN               (10)
+#define configMAX_TASK_NAME_LEN               (16)
 #define configUSE_16_BIT_TICKS                0
 
 /* Software timer definitions. */
 #define configUSE_TIMERS                      0
 #define configTIMER_TASK_PRIORITY             2
 #define configTIMER_QUEUE_LENGTH              5
-#define configTIMER_TASK_STACK_DEPTH          (configMINIMAL_STACK_SIZE * 2)
+#define configTIMER_TASK_STACK_DEPTH          (configMINIMAL_STACK_SIZE)
 
 /* Constants that build features in or out. */
-#define configUSE_MUTEXES                     1
-#define configUSE_RECURSIVE_MUTEXES           1
-#define configUSE_COUNTING_SEMAPHORES         1
-#define configUSE_QUEUE_SETS                  1
-#define configUSE_TASK_NOTIFICATIONS          1
-#define configUSE_TRACE_FACILITY              1
-#define configUSE_TICKLESS_IDLE               1
+#define configUSE_MUTEXES                     0
+#define configUSE_RECURSIVE_MUTEXES           0
+#define configUSE_COUNTING_SEMAPHORES         0
+#define configUSE_QUEUE_SETS                  0
+#define configUSE_TASK_NOTIFICATIONS          0
+#define configUSE_TRACE_FACILITY              0
+#define configUSE_TICKLESS_IDLE               0
 #define configUSE_APPLICATION_TASK_TAG        0
 #define configUSE_NEWLIB_REENTRANT            0
 #define configUSE_CO_ROUTINES                 0
@@ -88,8 +88,8 @@ extern uint32_t SystemCoreClock;
 #define configASSERT( x )                     if( ( x ) == 0 ) { taskDISABLE_INTERRUPTS(); for( ;; ); }
 
 /* Constants that define which hook (callback) functions should be used. */
-#define configUSE_IDLE_HOOK                   0
-#define configUSE_TICK_HOOK                   0
+#define configUSE_IDLE_HOOK                   1
+#define configUSE_TICK_HOOK                   1
 #define configUSE_DAEMON_TASK_STARTUP_HOOK    0
 #define configUSE_MALLOC_FAILED_HOOK          0
 
@@ -97,7 +97,7 @@ extern uint32_t SystemCoreClock;
 #define configENABLE_MPU                      0
 #define configENABLE_FPU                      1
 #define configENABLE_TRUSTZONE                1
-#define configMINIMAL_SECURE_STACK_SIZE       ((uint32_t)1024)
+#define configMINIMAL_SECURE_STACK_SIZE       ((uint32_t)256)
 #define configRUN_FREERTOS_SECURE_ONLY        0
 
 /* Cortex-M specific definitions. */
@@ -153,9 +153,9 @@ extern uint32_t SystemCoreClock;
 #define vPortSVCHandler                       SVC_Handler
 #define xPortSysTickHandler                   SysTick_Handler
 
-#if (defined(__ARMCC_VERSION) || defined(__GNUC__) || defined(__ICCARM__))
-/* Include debug event definitions */
-#include "freertos_evr.h"
-#endif
+//#if (defined(__ARMCC_VERSION) || defined(__GNUC__) || defined(__ICCARM__))
+///* Include debug event definitions */
+//#include "freertos_evr.h"
+//#endif
 
 #endif /* FREERTOS_CONFIG_H */
--- main.c
+++ main.c
@@ -22,20 +22,13 @@ using namespace std;
 #include <xmc_flash.h>
 #include <xmc_vadc.h>
 
-#include <arm_math.h>
-
-#include "EventRecorder.h"
-
 #include "RTE_Components.h"
 
 #include "FreeRTOS.h"
 #include "task.h"
-#include "queue.h"
-#include "semphr.h"
-#include "timers.h"
 
-#include "custom_def.h"
 #include "led.h"
+#include "EventRecorder.h"
 
 static uint32_t tmpDts;
 static float tmpCel;
@@ -63,79 +56,16 @@ const XMC_UART_CH_CONFIG_t uart_config = {
 	.parity_mode = XMC_USIC_CH_PARITY_MODE_NONE
 };
 
-int stdout_putchar (int ch) {
+int stdout_putchar(int ch) {
 	XMC_UART_CH_Transmit(XMC_UART0_CH0, (uint8_t)ch);
 	return ch;
 }
 
-TaskHandle_t g_filter_task_handle;
-TaskHandle_t g_disturb_gen_task_handle;
-TaskHandle_t g_noise_gen_task_handle;
-TaskHandle_t g_sine_gen_task_handle;
 TaskHandle_t g_sync_task_handle;
 
-#define ENABLE_CONFIG 1
-// =============================
-//   <o>Oscillator Sampling Frequency [Hz] <1000-10000>
-//   <i> Set the oscillator sampling frequency.
-//   <i> Default: 5000  (5 KHz)
-#define SAMPLING_FREQ 1000  // generating task (5 KHz)
-
-//   <o>Noise Frequency [Hz] <50-10000>
-//   <i> Set the noise signal frequency.
-//   <i> Default: 1500 Hz
-#define NOISE_FREQ    50  // noise (2 KHz)
-
-//   <o>Signal Frequency [Hz] <10-1000>
-//   <i> Set the signal frequency.
-//   <i> Default: 330 Hz
-#define SIGNAL_FREQ    10  // disturbed signal (250 Hz)
-
-// </e>
-//------------- <<< end of configuration section >>> -----------------------
-
-
-//sine_generator_q15_t Signal_set;
-//sine_generator_q15_t Noise_set;
-
-q15_t sine;
-q15_t noise;
-q15_t disturbed;
-q15_t filtered;
-
-//void sine_gen(void) {
-//  while(1) { 
-//		xTaskNotifyWait( 0x00,      /* Don't clear any notification bits on entry. */
-//                         UINT32_MAX, /* Reset the notification value to 0 on exit. */
-//                         NULL,
-//                         portMAX_DELAY );  /* Block indefinitely. */
-
-////    sine = sine_calc_sample_q15(&Signal_set) / 2;
-
-////		xTaskNotify( g_noise_gen_task_handle, 0, eNoAction );
-//  }
-//}
-
-extern volatile uint32_t test_val32[3];
 void sync_tsk(void) {
-	TickType_t xLastWakeTime;
-  while(1) {			
-		
-//		xLastWakeTime = xTaskGetTickCount ();    
-//		xTaskNotify( g_sine_gen_task_handle, 0, eNoAction );
-//    vTaskDelayUntil( &xLastWakeTime, 10 / portTICK_PERIOD_MS );
-		
-		XMC_SCU_StartTemperatureMeasurement();		
-				
+  while(1) {							
 		printf("t:%u\n", xTaskGetTickCount());
-		//T_DTS = (RESULT - 605) / 2.05 [°C]
-		tmpDts = XMC_SCU_GetTemperatureMeasurement();
-		tmpCel = (tmpDts-605)/2.05;
-		printf("%.1f\n", tmpCel);
-
-		tmpV13 = XMC_SCU_POWER_GetEVR13Voltage();
-		tmpV33 = XMC_SCU_POWER_GetEVR33Voltage();
-		printf("%.1f %.1f\n", tmpV13, tmpV33);	
 		
 		#ifdef tskKERNEL_VERSION_NUMBER
 		printf("OS Ver:%s\n", tskKERNEL_VERSION_NUMBER);
@@ -147,17 +77,20 @@ void sync_tsk(void) {
 						
 		LED_Toggle(0);
 		LED_Toggle(1);
-//		xTaskNotify( g_sine_gen_task_handle, 0, eNoAction );
   }
 }
 
-#define TEST_BAUDRATE	(921600)
+void vApplicationIdleHook( void ) {
+//		XMC_UART_CH_Transmit(XMC_UART0_CH0, '#');
+}
+
+void vApplicationTickHook( void ) {	
+//	XMC_UART_CH_Transmit(XMC_UART0_CH0, '$');
+}
+
 int main(void) {
   EventRecorderInitialize(EventRecordAll, 1);
-
-	XMC_SCU_EnableTemperatureSensor();
-	XMC_SCU_StartTemperatureMeasurement();
-
+	
 	LED_Initialize();
 	
 	/*Initialize the UART driver */
@@ -173,83 +106,22 @@ int main(void) {
   /* Configure pins */
 	XMC_GPIO_Init(UART_TX, &uart_tx);
   XMC_GPIO_Init(UART_RX, &uart_rx);
+	  
+	printf ("XMC47 Relax @%u Hz %08X\n",
+	SystemCoreClock,
+	SCU_GENERAL->IDCHIP	);
 	
-	printf("XMC4700 ARMCC Test @ %u Hz\n", SystemCoreClock);
-
-	printf("%u Hz, %08X, CM:%d, FPU_USED:%d, SCU_IDCHIP:%08X\n",
-			SystemCoreClock, SCB->CPUID,
-			__CORTEX_M, __FPU_USED,
-			SCU_GENERAL->IDCHIP);
-	printf("Boot Mode:%u\n", XMC_SCU_GetBootMode());
-	
-  #ifdef RTE_Compiler_IO_STDOUT_User
-	printf("RTE_Compiler_IO_STDOUT_User\n");
-	#endif
-	
-  #ifdef RTE_Compiler_IO_STDOUT_EVR
-	printf("RTE_Compiler_IO_STDOUT_EVR\n");
-	#endif
-
-  #ifdef RTE_Compiler_IO_STDOUT_ITM
-	printf("RTE_Compiler_IO_STDOUT_ITM\n");
-	#endif
-	
-//	  // compute coefficients for IIR sine generators
-//  sine_generator_init_q15(&Signal_set, SIGNAL_FREQ, SAMPLING_FREQ);
-//  sine_generator_init_q15(&Noise_set, NOISE_FREQ, SAMPLING_FREQ);
-//  printf ("Sine Generator Initialised\n\r");
-
-  // initialize low pass filter
-//  low_pass_filter_init();
-//  printf ("Low Pass Filter Initialised\n\r");
-
-  // initialize the timing system to activate the four tasks 
-  // of the application program
-//		xTaskCreate((TaskFunction_t)filter_tsk,
-//							(const portCHAR *)"filter_tsk",
-//							384,
-//							NULL,
-//							2,
-//							&g_filter_task_handle);							
-//	printf ("filter_tsk Task Initialised\n\r");
-
-//		xTaskCreate((TaskFunction_t)disturb_gen,
-//							(const portCHAR *)"disturb_gen",
-//							384,
-//							NULL,
-//							2,
-//							&g_disturb_gen_task_handle);
-//	printf ("disturb_gen Task Initialised\n\r");
-//								xTaskCreate((TaskFunction_t)noise_gen,
-//							(const portCHAR *)"noise_gen",
-//							384,
-//							NULL,
-//							2,
-//							&g_noise_gen_task_handle);
-//  printf ("noise_gen Task Initialised\n\r");
-																			
-//	xTaskCreate((TaskFunction_t)sine_gen,
-//							(const portCHAR *)"sine_gen",
-//							256,
-//							NULL,
-//							tskIDLE_PRIORITY+2,
-//							&g_sine_gen_task_handle);							
-//  printf ("sine_gen Task Initialised\n\r");
-							
-							xTaskCreate((TaskFunction_t)sync_tsk,
+	xTaskCreate((TaskFunction_t)sync_tsk,
 							(const portCHAR *)"sync_tsk",
 							256,
 							NULL,
-							tskIDLE_PRIORITY+3,
+							configMAX_PRIORITIES-1,
 							&g_sync_task_handle);							
-//  printf ("sync_tsk Task Initialised\n\r");
-//  printf ("Application Running\n\r");
 								
   /* Start scheduler */  
 	vTaskStartScheduler();
 	
-//Should never come here
-							
+//Should never come here					
   while (1) {
 		__BKPT(0x99);
 	}
