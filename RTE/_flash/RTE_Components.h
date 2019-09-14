eee450cad43c9abc4dedf260090504a05d48486e
zhanzr <zhanzr@foxmail.com>
2019-09-14 23:35:03 +0800

    maintaince

--- RTE/_flash/RTE_Components.h
+++ RTE/_flash/RTE_Components.h
@@ -20,21 +20,21 @@
 #define RTE_DEVICE
 /*  Infineon::Device:Startup:2.2.4 */
 #define RTE_DEVICE_STARTUP
-/*  Infineon::Device:XMClib:GPIO:2.1.22 */
+/*  Infineon::Device:XMClib:GPIO:2.1.24 */
 #define RTE_DEVICE_XMCLIB_GPIO
-/*  Infineon::Device:XMClib:SCU:2.1.22 */
+/*  Infineon::Device:XMClib:SCU:2.1.24 */
 #define RTE_DEVICE_XMCLIB_SCU
-/*  Infineon::Device:XMClib:UART:2.1.22 */
+/*  Infineon::Device:XMClib:UART:2.1.24 */
 #define RTE_DEVICE_XMCLIB_UART
-/*  Keil.ARM Compiler::Compiler:I/O:STDERR:User:1.2.0 */
+/*  Keil.ARM Compiler::Compiler:I/O:STDERR:ITM:1.2.0 */
 #define RTE_Compiler_IO_STDERR          /* Compiler I/O: STDERR */
-          #define RTE_Compiler_IO_STDERR_User     /* Compiler I/O: STDERR User */
-/*  Keil.ARM Compiler::Compiler:I/O:STDOUT:User:1.2.0 */
+          #define RTE_Compiler_IO_STDERR_ITM      /* Compiler I/O: STDERR ITM */
+/*  Keil.ARM Compiler::Compiler:I/O:STDOUT:ITM:1.2.0 */
 #define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
-          #define RTE_Compiler_IO_STDOUT_User     /* Compiler I/O: STDOUT User */
-/*  Keil.ARM Compiler::Compiler:I/O:TTY:User:1.2.0 */
+          #define RTE_Compiler_IO_STDOUT_ITM      /* Compiler I/O: STDOUT ITM */
+/*  Keil.ARM Compiler::Compiler:I/O:TTY:ITM:1.2.0 */
 #define RTE_Compiler_IO_TTY             /* Compiler I/O: TTY */
-          #define RTE_Compiler_IO_TTY_User        /* Compiler I/O: TTY User */
+          #define RTE_Compiler_IO_TTY_ITM         /* Compiler I/O: TTY ITM */
 
 
 #endif /* RTE_COMPONENTS_H */
